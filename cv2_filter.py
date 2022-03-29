dc761b747526a9868edf3cb039230a8eede3a314
Pythonimeislabor <100678996+Pythonimeislabor@users.noreply.github.com>
2022-03-29 07:40:03 +0200

    Update cv2_filter.py

--- cv2_filter.py
+++ cv2_filter.py
@@ -5,7 +5,6 @@ kamera = cv2.VideoCapture(0)
 
 for _ in range(2000):
     _, bild = kamera.read()
-    cv2.imshow("ungefiltert", bild)
     
     bild = bild / 255
     filter_3x3 = [
@@ -25,7 +24,10 @@ for _ in range(2000):
     neues_bild2 = cv2.filter2D(bild, -1, filter_3x3)
     
     neues_bild_summe = (neues_bild2 + neues_bild1) / 2
-    cv2.imshow("filter_3x3", neues_bild_summe)
+    
+    filter_altes_bild = (neues_bild_summe * 3 + bild * 1) / 4
+    
+    cv2.imshow("filter_3x3", filter_altes_bild)
     cv2.waitKey(30)
     
 kamera.release()
