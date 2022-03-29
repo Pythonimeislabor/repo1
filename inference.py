809fd8aca07009350b24ad7d8214716c3e3c542a
Pythonimeislabor <100678996+Pythonimeislabor@users.noreply.github.com>
2022-03-29 14:22:02 +0200

    Update inference.py

--- inference.py
+++ inference.py
@@ -3,19 +3,23 @@ import tensorflow as tf
 import numpy as np
 import pigpio
 
+
+pi = pigpio.pi()
+pi.set_servo_pulsewidth(14,2500)
+                        
+
 model = tf.keras.models.load_model("model.h5")
 model.summary()
 kamera = cv2.VideoCapture(0)
 
-pi = pigpio.pi()
 
 def interpoliere(zahl):
     reichweite = 2000
     schrittweite = reichweite / 10
     return schrittweite * zahl + 500
     
-pi.set_servo_pulsewidth(14,2500)
-for _ in range(1000):
+
+for _ in range(10000):
     _, frame = kamera.read()
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     frame = cv2.blur(frame,(25,25))
@@ -25,12 +29,17 @@ for _ in range(1000):
     frame = frame / np.max(frame)
     cv2.imshow("",frame)
     key =  cv2.waitKey(30)
+    if key == 27:
+        break
 
     
     batch = np.reshape(frame, (1,28,28))
     ergebnis = model(batch)
+    
+    
     zahl = np.argmax(ergebnis[0])
-    print(zahl)
+    print("\r",zahl,end="  ")
+    
     pi.set_servo_pulsewidth(14,interpoliere(zahl))
 
 kamera.release()
